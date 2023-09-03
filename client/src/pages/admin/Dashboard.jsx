0d95a50a0e8c3afcaa7f6efdcf6256220e64b986
NEWUSER <duy9117@gmai.com>
2023-09-03 13:27:59 +0700

    Add chart and create dashboard admin

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BarChart from "../../components/chart/BarChart";
+import LineChart from "../../components/chart/LineChart";
+import {UserData, UserData2} from "../../ultils/constant";
+import {useSelector} from 'react-redux'
+import {apiGetUsers} from '../../apis/user';
+
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    const { posts } = useSelector((state) => state.post);
+    const { categories } = useSelector((state) => state.app);
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await apiGetUsers();
+            if(response.status) {
+                setUsers(response.users);
+            }
+        }
+
+        fetchUsers();
+    })
+
+    const [userData, setUserData] = useState({
+        labels: UserData.map((data) => data.year),
+        datasets: [
+            {
+                label: "Users Gained",
+                data: UserData.map((data) => data.userGain),
+                backgroundColor: ["rgba(247, 92, 30, 0.6)"],
+                borderColor: "black",
+                borderWidth: 2,
+            },
+            {
+                label: "Users Gained 2",
+                data: UserData2.map((data) => data.userGain),
+                backgroundColor: ["rgba(230,0,35, 0.6)"],
+                borderColor: "black",
+                borderWidth: 2,
+            },
+        ]
+    })
+
+    return <div className="px-[30px] py-[20px]">
+        <p className="text-[30px] font-bold mb-[20px]">Dashboard Admin</p>
+        <div className="grid grid-cols-3 h-[200px] gap-[30px] mb-[30px]">
+            <div className="bg-red-400 flex items-center justify-center flex-col">
+                <p className="text-white text-[30px] font-bold mb-[20px]">Users</p>
+                <p className="text-white text-[40px] font-bold mb-[20px]">{users?.length}</p>
+            </div>
+            <div className="bg-red-400 flex items-center justify-center flex-col">
+                <p className="text-white text-[30px] font-bold mb-[20px]">Categories</p>
+                <p className="text-white text-[40px] font-bold mb-[20px]">{categories?.length}</p>
+            </div>
+            <div className="bg-red-400 flex items-center justify-center flex-col">
+                <p className="text-white text-[30px] font-bold mb-[20px]">Posts</p>
+                <p className="text-white text-[40px] font-bold mb-[20px]">{posts?.length}</p>
+            </div>
+        </div>
+        <div className="grid grid-cols-2 h-[320px] gap-[30px]">
+            <div className="bg-green-400 px-[20px] pt-[40px]">
+                <BarChart chartData={userData} />
+            </div>
+            <div className="bg-green-400 px-[20px] pt-[40px]">
+                <LineChart chartData={userData} />
+            </div>
+        </div>
+    </div>;
 };
 
 export default Dashboard;
--- client/src/ultils/constant.js
+++ client/src/ultils/constant.js
@@ -9,3 +9,81 @@ export const roles = [
     },
 ]
 
+export const UserData = [
+    {
+        id: 1,
+        year: 2020,
+        userGain: 25350,
+        userLost: 643,
+    },
+    {
+        id: 2,
+        year: 2022,
+        userGain: 53423,
+        userLost: 867,
+    },
+    {
+        id: 3,
+        year: 2023,
+        userGain: 35646,
+        userLost: 234,
+    },
+    {
+        id: 4,
+        year: 2021,
+        userGain: 57534,
+        userLost: 234,
+    },
+    {
+        id: 5,
+        year: 2022,
+        userGain: 16546,
+        userLost: 324,
+    },
+    {
+        id: 6,
+        year: 2023,
+        userGain: 54367,
+        userLost: 534,
+    },
+]
+
+export const UserData2 = [
+    {
+        id: 1,
+        year: 2020,
+        userGain: 43543,
+        userLost: 543,
+    },
+    {
+        id: 2,
+        year: 2022,
+        userGain: 34765,
+        userLost: 565,
+    },
+    {
+        id: 3,
+        year: 2023,
+        userGain: 42553,
+        userLost: 432,
+    },
+    {
+        id: 4,
+        year: 2021,
+        userGain: 24356,
+        userLost: 234,
+    },
+    {
+        id: 5,
+        year: 2022,
+        userGain: 65479,
+        userLost: 445,
+    },
+    {
+        id: 6,
+        year: 2023,
+        userGain: 12456,
+        userLost: 652,
+    },
+]
+
